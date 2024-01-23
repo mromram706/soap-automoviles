a0db4650521eb243839abf84745560e49a60af59
mromram706 <131792936+mromram706@users.noreply.github.com>
2024-01-23 10:54:40 +0100

    Update modelos.php

--- modelos.php
+++ modelos.php
@@ -1,20 +1,17 @@
 <?php
-// Incluir la clase GestionAutomovilesAuth para usarla
+
 require_once "GestionAutomovilesAuth.php";
 
-// Verificar si se ha proporcionado el parámetro 'marca'
 if (isset($_GET['marca'])) {
     $nombreMarca = $_GET['marca'];
 
-    // Instanciar la clase y obtener todas las marcas
     $client = new GestionAutomovilesAuth('http://dwes.infinityfreeapp.com/soap-automoviles/service-automoviles-auth.php');
     $marcas = $client->ObtenerMarcas();
 
-    // Buscar el ID que corresponde al nombre de la marca
     $marcaId = array_search($nombreMarca, $marcas);
 
     if ($marcaId !== false) {
-        // Obtener modelos para el ID de la marca encontrada
+        
         $modelos = $client->ObtenerModelos($marcaId);
 
         // Mostrar los modelos
